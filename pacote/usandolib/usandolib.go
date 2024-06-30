63584fc8f830e1285137e1b90e769f112f57c334
Gustavo Antonino <guh.antonino@gmail.com>
2024-06-30 00:29:12 -0300

    Aprendendo sobre pacotes

---
+++ pacote/usandolib/usandolib.go
@@ -0,0 +1,12 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"github.com/cod3rcursos/goarea"
+// 	"github.com/cod3rcursos/goarea/abc"
+// )
+
+// func main() {
+// 	fmt.Println(goarea.Circ(4.0))
+// 	abc.Epa()
+// }
